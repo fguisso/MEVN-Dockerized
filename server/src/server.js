1a9296defdaa290ec53d83bba73f200b412aeb08
Fernando Guisso <fguisso@gmail.com>
2017-03-07 02:37:26 -0300

    server.js okay

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.set('port', process.env.PORT || 3000);
+app.set('host', process.env.HOST || 'localhost');
 
-const server = app.listen('3000', 'localhost', () =>
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+const server = app.listen(app.get('port'), app.get('host'), () =>
   console.log(`\nServer listening at http://${server.address().address}:${server.address().port}`));
