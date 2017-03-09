8ac26538441d5995876b5c48d74b462de9253d5b
Fernando Guisso <fguisso@gmail.com>
2017-03-09 00:18:30 -0300

    add routes in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import expressJwt from 'express-jwt';
 
 const app = express();
+
+const secret = process.env.SECRET;
 
 app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.HOST || 'localhost');
@@ -18,6 +21,16 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
+
+app.use('/users', expressJwt({ secret }))
+  .unless({ path: ['/users/auth', '/users/init', '/users/register'] });
+app.use('/items', expressJwt({ secret }));
+app.use('/info', expressJwt({ secret }));
+
+
+app.use('/users', require('./controllers/users.controller.js'));
+app.use('/items', require('./controllers/items.controller.js'));
+app.use('/info', require('./controllers/infos.controller.js'));
 
 const server = app.listen(app.get('port'), app.get('host'), () =>
   console.log(`\nServer listening at http://${server.address().address}:${server.address().port}`));
